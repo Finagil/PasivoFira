cac6fa044964abd40ff17ba91c0448fcd9c4c9f8
denisatux <denise.gonzalez@finagil.com.mx>
2018-05-04 18:24:55 -0500

    DAGL
    Se agregar funciones de eliminar, modificar y agregar ministraciones

--- modulo_contratos/frm_ministraciones.Designer.vb
+++ modulo_contratos/frm_ministraciones.Designer.vb
@@ -23,11 +23,9 @@ Partial Class frm_ministraciones
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
-        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
-        Dim DataGridViewCellStyle3 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
-        Me.CONT_CPF_ministracionesBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-        Me.DS_contratos = New Fira_Cartera_Pasiva.DS_contratos()
+        Dim DataGridViewCellStyle4 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle5 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle6 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Me.CONT_CPF_ministracionesDataGridView = New System.Windows.Forms.DataGridView()
         Me.DataGridViewTextBoxColumn4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.DataGridViewTextBoxColumn3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
@@ -37,11 +35,96 @@ Partial Class frm_ministraciones
         Me.DataGridViewTextBoxColumn7 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.DataGridViewTextBoxColumn9 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.DataGridViewTextBoxColumn10 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.CONT_CPF_ministracionesBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.DS_contratos = New Fira_Cartera_Pasiva.DS_contratos()
+        Me.bt_agregar = New System.Windows.Forms.Button()
+        Me.bt_modificar = New System.Windows.Forms.Button()
+        Me.bt_eliminar = New System.Windows.Forms.Button()
+        Me.txt_id = New System.Windows.Forms.TextBox()
         Me.CONT_CPF_ministracionesTableAdapter = New Fira_Cartera_Pasiva.DS_contratosTableAdapters.CONT_CPF_ministracionesTableAdapter()
+        Me.CONTCPFcontratosBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.CONT_CPF_contratosTableAdapter = New Fira_Cartera_Pasiva.DS_contratosTableAdapters.CONT_CPF_contratosTableAdapter()
+        CType(Me.CONT_CPF_ministracionesDataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.CONT_CPF_ministracionesBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.DS_contratos, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.CONT_CPF_ministracionesDataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.CONTCPFcontratosBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'CONT_CPF_ministracionesDataGridView
+        '
+        Me.CONT_CPF_ministracionesDataGridView.AllowUserToDeleteRows = False
+        Me.CONT_CPF_ministracionesDataGridView.AutoGenerateColumns = False
+        Me.CONT_CPF_ministracionesDataGridView.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.DataGridViewTextBoxColumn4, Me.DataGridViewTextBoxColumn3, Me.DataGridViewTextBoxColumn2, Me.DataGridViewTextBoxColumn5, Me.DataGridViewTextBoxColumn6, Me.DataGridViewTextBoxColumn7, Me.DataGridViewTextBoxColumn9, Me.DataGridViewTextBoxColumn10})
+        Me.CONT_CPF_ministracionesDataGridView.DataSource = Me.CONT_CPF_ministracionesBindingSource
+        Me.CONT_CPF_ministracionesDataGridView.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter
+        Me.CONT_CPF_ministracionesDataGridView.Location = New System.Drawing.Point(5, 3)
+        Me.CONT_CPF_ministracionesDataGridView.Name = "CONT_CPF_ministracionesDataGridView"
+        Me.CONT_CPF_ministracionesDataGridView.Size = New System.Drawing.Size(699, 164)
+        Me.CONT_CPF_ministracionesDataGridView.TabIndex = 10
+        '
+        'DataGridViewTextBoxColumn4
+        '
+        Me.DataGridViewTextBoxColumn4.DataPropertyName = "consecutivo"
+        Me.DataGridViewTextBoxColumn4.HeaderText = "Id"
+        Me.DataGridViewTextBoxColumn4.Name = "DataGridViewTextBoxColumn4"
+        Me.DataGridViewTextBoxColumn4.ToolTipText = "Consecutivo"
+        Me.DataGridViewTextBoxColumn4.Width = 30
+        '
+        'DataGridViewTextBoxColumn3
+        '
+        Me.DataGridViewTextBoxColumn3.DataPropertyName = "fecha"
+        Me.DataGridViewTextBoxColumn3.HeaderText = "Fecha"
+        Me.DataGridViewTextBoxColumn3.Name = "DataGridViewTextBoxColumn3"
+        Me.DataGridViewTextBoxColumn3.Width = 70
+        '
+        'DataGridViewTextBoxColumn2
+        '
+        Me.DataGridViewTextBoxColumn2.DataPropertyName = "monto"
+        DataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight
+        DataGridViewCellStyle4.Format = "N2"
+        DataGridViewCellStyle4.NullValue = Nothing
+        Me.DataGridViewTextBoxColumn2.DefaultCellStyle = DataGridViewCellStyle4
+        Me.DataGridViewTextBoxColumn2.HeaderText = "Monto"
+        Me.DataGridViewTextBoxColumn2.Name = "DataGridViewTextBoxColumn2"
+        '
+        'DataGridViewTextBoxColumn5
+        '
+        Me.DataGridViewTextBoxColumn5.DataPropertyName = "porcentaje_cobro"
+        Me.DataGridViewTextBoxColumn5.HeaderText = "% Cobro"
+        Me.DataGridViewTextBoxColumn5.Name = "DataGridViewTextBoxColumn5"
+        Me.DataGridViewTextBoxColumn5.Width = 70
+        '
+        'DataGridViewTextBoxColumn6
+        '
+        Me.DataGridViewTextBoxColumn6.DataPropertyName = "iva"
+        DataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight
+        DataGridViewCellStyle5.Format = "N2"
+        Me.DataGridViewTextBoxColumn6.DefaultCellStyle = DataGridViewCellStyle5
+        Me.DataGridViewTextBoxColumn6.HeaderText = "IVA"
+        Me.DataGridViewTextBoxColumn6.Name = "DataGridViewTextBoxColumn6"
+        '
+        'DataGridViewTextBoxColumn7
+        '
+        Me.DataGridViewTextBoxColumn7.DataPropertyName = "importe"
+        DataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight
+        DataGridViewCellStyle6.Format = "N2"
+        Me.DataGridViewTextBoxColumn7.DefaultCellStyle = DataGridViewCellStyle6
+        Me.DataGridViewTextBoxColumn7.HeaderText = "Importe"
+        Me.DataGridViewTextBoxColumn7.Name = "DataGridViewTextBoxColumn7"
+        '
+        'DataGridViewTextBoxColumn9
+        '
+        Me.DataGridViewTextBoxColumn9.DataPropertyName = "estatus"
+        Me.DataGridViewTextBoxColumn9.HeaderText = "Estatus"
+        Me.DataGridViewTextBoxColumn9.Name = "DataGridViewTextBoxColumn9"
+        '
+        'DataGridViewTextBoxColumn10
+        '
+        Me.DataGridViewTextBoxColumn10.DataPropertyName = "fe_descuento"
+        Me.DataGridViewTextBoxColumn10.HeaderText = "Descuento"
+        Me.DataGridViewTextBoxColumn10.Name = "DataGridViewTextBoxColumn10"
+        Me.DataGridViewTextBoxColumn10.ToolTipText = "Fecha descuento Fira"
+        Me.DataGridViewTextBoxColumn10.Width = 80
         '
         'CONT_CPF_ministracionesBindingSource
         '
@@ -53,119 +136,90 @@ Partial Class frm_ministraciones
         Me.DS_contratos.DataSetName = "DS_contratos"
         Me.DS_contratos.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
-        'CONT_CPF_ministracionesDataGridView
+        'bt_agregar
         '
-        Me.CONT_CPF_ministracionesDataGridView.AllowUserToAddRows = False
-        Me.CONT_CPF_ministracionesDataGridView.AllowUserToDeleteRows = False
-        Me.CONT_CPF_ministracionesDataGridView.AutoGenerateColumns = False
-        Me.CONT_CPF_ministracionesDataGridView.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.DataGridViewTextBoxColumn4, Me.DataGridViewTextBoxColumn3, Me.DataGridViewTextBoxColumn2, Me.DataGridViewTextBoxColumn5, Me.DataGridViewTextBoxColumn6, Me.DataGridViewTextBoxColumn7, Me.DataGridViewTextBoxColumn9, Me.DataGridViewTextBoxColumn10})
-        Me.CONT_CPF_ministracionesDataGridView.DataSource = Me.CONT_CPF_ministracionesBindingSource
-        Me.CONT_CPF_ministracionesDataGridView.Location = New System.Drawing.Point(5, 3)
-        Me.CONT_CPF_ministracionesDataGridView.Name = "CONT_CPF_ministracionesDataGridView"
-        Me.CONT_CPF_ministracionesDataGridView.ReadOnly = True
-        Me.CONT_CPF_ministracionesDataGridView.Size = New System.Drawing.Size(699, 178)
-        Me.CONT_CPF_ministracionesDataGridView.TabIndex = 10
+        Me.bt_agregar.Location = New System.Drawing.Point(410, 187)
+        Me.bt_agregar.Name = "bt_agregar"
+        Me.bt_agregar.Size = New System.Drawing.Size(81, 23)
+        Me.bt_agregar.TabIndex = 148
+        Me.bt_agregar.Text = "Agregar"
+        Me.bt_agregar.UseVisualStyleBackColor = True
         '
-        'DataGridViewTextBoxColumn4
+        'bt_modificar
         '
-        Me.DataGridViewTextBoxColumn4.DataPropertyName = "consecutivo"
-        Me.DataGridViewTextBoxColumn4.HeaderText = "Id"
-        Me.DataGridViewTextBoxColumn4.Name = "DataGridViewTextBoxColumn4"
-        Me.DataGridViewTextBoxColumn4.ReadOnly = True
-        Me.DataGridViewTextBoxColumn4.ToolTipText = "Consecutivo"
-        Me.DataGridViewTextBoxColumn4.Width = 30
+        Me.bt_modificar.Location = New System.Drawing.Point(497, 187)
+        Me.bt_modificar.Name = "bt_modificar"
+        Me.bt_modificar.Size = New System.Drawing.Size(81, 23)
+        Me.bt_modificar.TabIndex = 149
+        Me.bt_modificar.Text = "Modificar"
+        Me.bt_modificar.UseVisualStyleBackColor = True
         '
-        'DataGridViewTextBoxColumn3
+        'bt_eliminar
         '
-        Me.DataGridViewTextBoxColumn3.DataPropertyName = "fecha"
-        Me.DataGridViewTextBoxColumn3.HeaderText = "Fecha"
-        Me.DataGridViewTextBoxColumn3.Name = "DataGridViewTextBoxColumn3"
-        Me.DataGridViewTextBoxColumn3.ReadOnly = True
-        Me.DataGridViewTextBoxColumn3.Width = 70
+        Me.bt_eliminar.Location = New System.Drawing.Point(584, 187)
+        Me.bt_eliminar.Name = "bt_eliminar"
+        Me.bt_eliminar.Size = New System.Drawing.Size(81, 23)
+        Me.bt_eliminar.TabIndex = 150
+        Me.bt_eliminar.Text = "Eliminar"
+        Me.bt_eliminar.UseVisualStyleBackColor = True
         '
-        'DataGridViewTextBoxColumn2
+        'txt_id
         '
-        Me.DataGridViewTextBoxColumn2.DataPropertyName = "monto"
-        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight
-        DataGridViewCellStyle1.Format = "N2"
-        DataGridViewCellStyle1.NullValue = Nothing
-        Me.DataGridViewTextBoxColumn2.DefaultCellStyle = DataGridViewCellStyle1
-        Me.DataGridViewTextBoxColumn2.HeaderText = "Monto"
-        Me.DataGridViewTextBoxColumn2.Name = "DataGridViewTextBoxColumn2"
-        Me.DataGridViewTextBoxColumn2.ReadOnly = True
-        '
-        'DataGridViewTextBoxColumn5
-        '
-        Me.DataGridViewTextBoxColumn5.DataPropertyName = "porcentaje_cobro"
-        Me.DataGridViewTextBoxColumn5.HeaderText = "% Cobro"
-        Me.DataGridViewTextBoxColumn5.Name = "DataGridViewTextBoxColumn5"
-        Me.DataGridViewTextBoxColumn5.ReadOnly = True
-        Me.DataGridViewTextBoxColumn5.Width = 70
-        '
-        'DataGridViewTextBoxColumn6
-        '
-        Me.DataGridViewTextBoxColumn6.DataPropertyName = "iva"
-        DataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight
-        DataGridViewCellStyle2.Format = "N2"
-        Me.DataGridViewTextBoxColumn6.DefaultCellStyle = DataGridViewCellStyle2
-        Me.DataGridViewTextBoxColumn6.HeaderText = "IVA"
-        Me.DataGridViewTextBoxColumn6.Name = "DataGridViewTextBoxColumn6"
-        Me.DataGridViewTextBoxColumn6.ReadOnly = True
-        '
-        'DataGridViewTextBoxColumn7
-        '
-        Me.DataGridViewTextBoxColumn7.DataPropertyName = "importe"
-        DataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight
-        DataGridViewCellStyle3.Format = "N2"
-        Me.DataGridViewTextBoxColumn7.DefaultCellStyle = DataGridViewCellStyle3
-        Me.DataGridViewTextBoxColumn7.HeaderText = "Importe"
-        Me.DataGridViewTextBoxColumn7.Name = "DataGridViewTextBoxColumn7"
-        Me.DataGridViewTextBoxColumn7.ReadOnly = True
-        '
-        'DataGridViewTextBoxColumn9
-        '
-        Me.DataGridViewTextBoxColumn9.DataPropertyName = "estatus"
-        Me.DataGridViewTextBoxColumn9.HeaderText = "Estatus"
-        Me.DataGridViewTextBoxColumn9.Name = "DataGridViewTextBoxColumn9"
-        Me.DataGridViewTextBoxColumn9.ReadOnly = True
-        '
-        'DataGridViewTextBoxColumn10
-        '
-        Me.DataGridViewTextBoxColumn10.DataPropertyName = "fe_descuento"
-        Me.DataGridViewTextBoxColumn10.HeaderText = "Descuento"
-        Me.DataGridViewTextBoxColumn10.Name = "DataGridViewTextBoxColumn10"
-        Me.DataGridViewTextBoxColumn10.ReadOnly = True
-        Me.DataGridViewTextBoxColumn10.ToolTipText = "Fecha descuento Fira"
-        Me.DataGridViewTextBoxColumn10.Width = 80
+        Me.txt_id.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.CONT_CPF_ministracionesBindingSource, "id_ministracion", True))
+        Me.txt_id.Location = New System.Drawing.Point(102, 124)
+        Me.txt_id.Name = "txt_id"
+        Me.txt_id.Size = New System.Drawing.Size(100, 20)
+        Me.txt_id.TabIndex = 151
         '
         'CONT_CPF_ministracionesTableAdapter
         '
         Me.CONT_CPF_ministracionesTableAdapter.ClearBeforeFill = True
         '
+        'CONTCPFcontratosBindingSource
+        '
+        Me.CONTCPFcontratosBindingSource.DataMember = "CONT_CPF_contratos"
+        Me.CONTCPFcontratosBindingSource.DataSource = Me.DS_contratos
+        '
+        'CONT_CPF_contratosTableAdapter
+        '
+        Me.CONT_CPF_contratosTableAdapter.ClearBeforeFill = True
+        '
         'frm_ministraciones
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(709, 189)
+        Me.ClientSize = New System.Drawing.Size(710, 219)
+        Me.Controls.Add(Me.bt_eliminar)
+        Me.Controls.Add(Me.bt_modificar)
+        Me.Controls.Add(Me.bt_agregar)
         Me.Controls.Add(Me.CONT_CPF_ministracionesDataGridView)
+        Me.Controls.Add(Me.txt_id)
         Me.Name = "frm_ministraciones"
         Me.Text = "Ministraciones"
+        CType(Me.CONT_CPF_ministracionesDataGridView, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.CONT_CPF_ministracionesBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.DS_contratos, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.CONT_CPF_ministracionesDataGridView, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.CONTCPFcontratosBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents DS_contratos As Fira_Cartera_Pasiva.DS_contratos
     Friend WithEvents CONT_CPF_ministracionesBindingSource As System.Windows.Forms.BindingSource
     Friend WithEvents CONT_CPF_ministracionesTableAdapter As Fira_Cartera_Pasiva.DS_contratosTableAdapters.CONT_CPF_ministracionesTableAdapter
     Friend WithEvents CONT_CPF_ministracionesDataGridView As System.Windows.Forms.DataGridView
-    Friend WithEvents DataGridViewTextBoxColumn4 As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents DataGridViewTextBoxColumn3 As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents DataGridViewTextBoxColumn2 As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents DataGridViewTextBoxColumn5 As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents DataGridViewTextBoxColumn6 As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents DataGridViewTextBoxColumn7 As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents DataGridViewTextBoxColumn9 As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents DataGridViewTextBoxColumn10 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents bt_agregar As Button
+    Friend WithEvents bt_modificar As Button
+    Friend WithEvents bt_eliminar As Button
+    Friend WithEvents CONTCPFcontratosBindingSource As BindingSource
+    Friend WithEvents CONT_CPF_contratosTableAdapter As DS_contratosTableAdapters.CONT_CPF_contratosTableAdapter
+    Friend WithEvents DataGridViewTextBoxColumn4 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn3 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn2 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn5 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn6 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn7 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn9 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn10 As DataGridViewTextBoxColumn
+    Friend WithEvents txt_id As TextBox
 End Class
--- modulo_contratos/frm_ministraciones.vb
+++ modulo_contratos/frm_ministraciones.vb
@@ -2,13 +2,156 @@ Public Class frm_ministraciones
 
 
     Private Sub frm_ministraciones_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: esta línea de código carga datos en la tabla 'DS_contratos.CONT_CPF_contratos' Puede moverla o quitarla según sea necesario.
+        '    Me.CONT_CPF_contratosTableAdapter.Fill(Me.DS_contratos.CONT_CPF_contratos)
         'TODO: This line of code loads data into the 'DS_contratos.CONT_CPF_ministraciones' table. You can move, or remove it, as needed.
-       
+
         Me.CONT_CPF_ministracionesTableAdapter.FillByCONTRATO(Me.DS_contratos.CONT_CPF_ministraciones, frm_contratos_alta.id_contrato)
+
+    End Sub
+
+    Private Sub bt_agregar_Click(sender As Object, e As EventArgs) Handles bt_agregar.Click
+        Dim fecha As Date
+        Dim monto As Decimal = 0
+        Dim iva As Decimal = 0
+        Dim porcentaje As Decimal = 0
+        Dim importe As Decimal = 0
+        Dim descuento As Date
+        Dim consecutivo As Integer = 0
+        Dim estatus As String
+        Dim smonto As Decimal = 0
+        Dim fechaaux As Date
+
+        'SUMAR MONTO TOTAL Y VALIDAD QUE NO EXCEDA EL MONTO FINANCIADO
+        Dim MontoFinanciado As Decimal = 0
+        MontoFinanciado = Me.CONT_CPF_contratosTableAdapter.monto_contrato(frm_contratos_alta.id_contrato)
+
+        For Renglones As Integer = 0 To CONT_CPF_ministracionesDataGridView.RowCount - 2
+            smonto = smonto + Me.CONT_CPF_ministracionesDataGridView.Item(2, Renglones).Value
+        Next
+
+        If MontoFinanciado <> smonto Then
+            MessageBox.Show("La suma del monto " & monto & " no coincide con el monto Financiado: " & MontoFinanciado, "MINISTRACIONES", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            ' MsgBox("La suma del monto " & monto & " no coincide con el monto Financiado: " & MontoFinanciado)
+        Else
+
+            Me.CONT_CPF_ministracionesTableAdapter.deletreministracion(frm_contratos_alta.id_contrato)
+
+            For Renglones As Integer = 0 To CONT_CPF_ministracionesDataGridView.RowCount - 2
+                consecutivo = consecutivo + 1
+                fecha = Me.CONT_CPF_ministracionesDataGridView.Item(1, Renglones).Value
+                If Renglones <> 0 Then
+                    fechaaux = Me.CONT_CPF_ministracionesDataGridView.Item(1, Renglones - 1).Value
+                    If fechaaux > fecha Then
+                        MessageBox.Show("Error en la fecha programada", "MINISTRACIONES", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                        Exit Sub
+                    End If
+                End If
+
+                monto = Me.CONT_CPF_ministracionesDataGridView.Item(2, Renglones).Value
+                porcentaje = Me.CONT_CPF_ministracionesDataGridView.Item(3, Renglones).Value
+                iva = Me.CONT_CPF_ministracionesDataGridView.Item(4, Renglones).Value
+                importe = Me.CONT_CPF_ministracionesDataGridView.Item(5, Renglones).Value
+                estatus = Me.CONT_CPF_ministracionesDataGridView.Item(6, Renglones).Value
+                descuento = Me.CONT_CPF_ministracionesDataGridView.Item(7, Renglones).Value
+
+
+
+                Me.CONT_CPF_ministracionesTableAdapter.InsertQueryMinistracion(monto, fecha, consecutivo, porcentaje, iva, importe, frm_contratos_alta.id_contrato, estatus, descuento)
+
+            Next
+            MessageBox.Show("se agrego ministracion correctamente", "MINISTRACIONES", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+
+            'Me.CONT_CPF_ministracionesDataGridView.DataBindings.Clear()
+
+            '     Me.CONT_CPF_ministracionesDataGridView.DataSource = Nothing
+            '    Me.CONT_CPF_ministracionesTableAdapter.FillByCONTRATO(Me.DS_contratos.CONT_CPF_ministraciones, frm_contratos_alta.id_contrato)
+        End If
 
     End Sub
 
     Private Sub CONT_CPF_ministracionesDataGridView_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles CONT_CPF_ministracionesDataGridView.CellContentClick
 
+    End Sub
+
+    Private Sub bt_eliminar_Click(sender As Object, e As EventArgs) Handles bt_eliminar.Click
+        ' deleterenglonmin
+        Dim id As Integer
+        id = txt_id.Text
+        Me.CONT_CPF_ministracionesTableAdapter.borrarMIN(id)
+        'Me.CONT_CPF_ministracionesDataGridView.DataSource = Nothing
+        'Me.CONT_CPF_ministracionesTableAdapter.FillByCONTRATO(Me.DS_contratos.CONT_CPF_ministraciones, frm_contratos_alta.id_contrato)
+        MessageBox.Show("se eliminó ministracion correctamente", "MINISTRACIONES", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+    End Sub
+
+    Private Sub bt_modificar_Click(sender As Object, e As EventArgs) Handles bt_modificar.Click
+        Dim fecha As Date
+        Dim monto As Decimal = 0
+        Dim iva As Decimal = 0
+        Dim porcentaje As Decimal = 0
+        Dim importe As Decimal = 0
+        Dim descuento As Date
+        Dim consecutivo As Integer = 0
+        Dim estatus As String
+        Dim smonto As Decimal = 0
+        Dim fechaaux As Date
+
+        'SUMAR MONTO TOTAL Y VALIDAD QUE NO EXCEDA EL MONTO FINANCIADO
+        Dim MontoFinanciado As Decimal = 0
+        MontoFinanciado = Me.CONT_CPF_contratosTableAdapter.monto_contrato(frm_contratos_alta.id_contrato)
+
+        For Renglones As Integer = 0 To CONT_CPF_ministracionesDataGridView.RowCount - 2
+            smonto = smonto + Me.CONT_CPF_ministracionesDataGridView.Item(2, Renglones).Value
+        Next
+
+        If MontoFinanciado <> smonto Then
+            MessageBox.Show("La suma del monto " & monto & " no coincide con el monto Financiado: " & MontoFinanciado, "MINISTRACIONES", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            ' MsgBox("La suma del monto " & monto & " no coincide con el monto Financiado: " & MontoFinanciado)
+        Else
+
+            ' Me.CONT_CPF_ministracionesTableAdapter.deletreministracion(frm_contratos_alta.id_contrato)
+
+            '  For Renglones As Integer = 0 To CONT_CPF_ministracionesDataGridView.RowCount - 2
+
+            '                smonto = smonto + Me.CONT_CPF_ministracionesDataGridView.Item(2, Renglones).Value
+            Dim r As Integer
+            r = Me.CONT_CPF_ministracionesDataGridView.CurrentRow.Index
+
+            'For Columnas As Integer = 0 To CONT_CPF_ministracionesDataGridView.ColumnCount - 1
+            '   consecutivo = consecutivo + 1
+            fecha = Me.CONT_CPF_ministracionesDataGridView.Item(1, r).Value
+            If r <> 0 Then
+                fechaaux = Me.CONT_CPF_ministracionesDataGridView.Item(1, r - 1).Value
+                If fechaaux > fecha Then
+                    MessageBox.Show("Error en la fecha programada", "MINISTRACIONES", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                    Exit Sub
+                End If
+            End If
+            monto = Me.CONT_CPF_ministracionesDataGridView.Item(2, r).Value
+            porcentaje = Me.CONT_CPF_ministracionesDataGridView.Item(3, r).Value
+            iva = Me.CONT_CPF_ministracionesDataGridView.Item(4, r).Value
+            importe = Me.CONT_CPF_ministracionesDataGridView.Item(5, r).Value
+            estatus = Me.CONT_CPF_ministracionesDataGridView.Item(6, r).Value
+            descuento = Me.CONT_CPF_ministracionesDataGridView.Item(7, r).Value
+            Dim idmin As Integer
+            idmin = txt_id.Text
+            Me.CONT_CPF_ministracionesTableAdapter.UpdateQueryMinistracion(monto, fecha, porcentaje, iva, importe, estatus, descuento, idmin)
+
+
+            '    Me.CONT_CPF_ministracionesTableAdapter.InsertQueryMinistracion(monto, fecha, consecutivo, porcentaje, iva, importe, frm_contratos_alta.id_contrato, estatus, descuento)
+            '  Next
+
+
+
+            'Next
+            MessageBox.Show("se modificó ministracion correctamente", "MINISTRACIONES", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            ' Me.Close()
+            '   Me.CONT_CPF_ministracionesTableAdapter.FillByCONTRATO(Me.DS_contratos.CONT_CPF_ministraciones, frm_contratos_alta.id_contrato)
+            '     Me.CONT_CPF_ministracionesDataGridView.DataSource = Nothing
+            ' Me.CONT_CPF_ministracionesTableAdapter
+            ' Me.CONT_CPF_ministracionesDataGridView.DataSource = Me.CONT_CPF_ministracionesTableAdapter.FillByCONTRATO(Me.DS_contratos.CONT_CPF_ministraciones, frm_contratos_alta.id_contrato)
+            Me.CONT_CPF_ministracionesDataGridView.DataBindings.Clear()
+            Me.CONT_CPF_ministracionesTableAdapter.FillByCONTRATO(Me.DS_contratos.CONT_CPF_ministraciones, frm_contratos_alta.id_contrato)
+        End If
     End Sub
 End Class
