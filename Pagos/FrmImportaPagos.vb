e1a414d0d9cbf3b5dff8ecb909d5e2b428bc0a90
ecacerest <ecacerest@finagil.com.mx>
2019-04-01 10:16:15 -0600

    ADD try para errores de ejecucion en importacion de pagos

--- Pagos/FrmImportaPagos.vb
+++ Pagos/FrmImportaPagos.vb
@@ -1,16 +1,20 @@
 ﻿Public Class FrmImportaPagos
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Cursor.Current = Cursors.WaitCursor
-        Dim p As New Process
-        p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
-        p.StartInfo.FileName = "F:\Executables\MOD_PasivoFiraCalculos.exe"
-        p.StartInfo.Arguments = "PAGOS"
-        p.Start()
-        p.WaitForExit()
-        Cursor.Current = Cursors.Default
-        MessageBox.Show("Pagos Importados.", "Importación de pagos", MessageBoxButtons.OK, MessageBoxIcon.Information)
-        Me.PagosFiraTableAdapter.Fill(PagosDS.PagosFira, False)
-        Boton()
+        Try
+            Cursor.Current = Cursors.WaitCursor
+            Dim p As New Process
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+            p.StartInfo.FileName = "F:\Executables\MOD_PasivoFiraCalculos.exe"
+            p.StartInfo.Arguments = "PAGOS"
+            p.Start()
+            p.WaitForExit()
+            Cursor.Current = Cursors.Default
+            MessageBox.Show("Pagos Importados.", "Importación de pagos", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Me.PagosFiraTableAdapter.Fill(PagosDS.PagosFira, False)
+            Boton()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error Pasivo Fira", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
     End Sub
 
     Private Sub FrmImportaPagos_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -19,15 +23,19 @@
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Cursor.Current = Cursors.WaitCursor
-        For Each r As PagosDS.PagosFiraRow In PagosDS.PagosFira.Rows
-            Me.PagosFiraTableAdapter.UpdatePagos(DTP_FechaFira.Value.Date, r.id_contrato, r.FechaAplicacion.ToString("yyyyMMdd"))
-        Next
-        Shell("F:\Executables\PsExec.exe \\192.168.29.41 -u AGIL\edgar-caceres -p c4c3r1t0s1 D:\Contratos$\Executables\MOD_PasivoFiraCalculos.exe PROCESA_PAGOS", AppWinStyle.Hide, False)
-        Me.PagosFiraTableAdapter.Fill(PagosDS.PagosFira, False)
-        Boton()
-        Cursor.Current = Cursors.Default
-        MessageBox.Show("En breve recibirás un correo de confirmación de terminación de proceso..", "Aplicación de pagos", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Try
+            Cursor.Current = Cursors.WaitCursor
+            For Each r As PagosDS.PagosFiraRow In PagosDS.PagosFira.Rows
+                Me.PagosFiraTableAdapter.UpdatePagos(DTP_FechaFira.Value.Date, r.id_contrato, r.FechaAplicacion.ToString("yyyyMMdd"))
+            Next
+            Shell("F:\Executables\PsExec.exe \\192.168.29.41 -u AGIL\edgar-caceres -p c4c3r1t0s1 D:\Contratos$\Executables\MOD_PasivoFiraCalculos.exe PROCESA_PAGOS", AppWinStyle.Hide, False)
+            Me.PagosFiraTableAdapter.Fill(PagosDS.PagosFira, False)
+            Boton()
+            Cursor.Current = Cursors.Default
+            MessageBox.Show("En breve recibirás un correo de confirmación de terminación de proceso..", "Aplicación de pagos", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error Pasivo Fira", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
     End Sub
 
     Sub Boton()
